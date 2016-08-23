9dd1c5c0124fe42dcc3475ed609ac2164050bbf8
mbd-s <michaelbsilverberg@gmail.com>
2016-08-23 15:33:57 +0200

    created seed data for orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
+
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Order.destroy_all
+orders = Order.create([{ mens_bikes: 0, womens_bikes: 2, kids_bikes: 1, invitation_sent: false, confirmed: false, notes: 'What time does the workshop start?'},
+  { mens_bikes: 1, womens_bikes: 0, kids_bikes: 0, invitation_sent: true, confirmed: true, notes: '' }])
+puts "Created #{Order.count} orders."
