019bb4afa7f651e94824eb296e79cd4bd94ef720
mbd-s <michaelbsilverberg@gmail.com>
2016-09-05 21:13:55 +0200

    orders can be saved

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,20 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
+  def create
+    @order = Order.create(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      flash[:error] = @orders.errors.full_messages.to_sentence
+      render :new
+      end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:mens_bikes, :womens_bikes, :kids_bikes, :notes, :invitation_sent, :confirmed)
+  end
+
 end
