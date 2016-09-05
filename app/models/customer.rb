bd7b58fa9c23173978261092ac6a90bee487207b
mbd-s <michaelbsilverberg@gmail.com>
2016-09-05 21:38:57 +0200

    validating stuff

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@ class Customer < ActiveRecord::Base
   has_many :orders, dependent: :destroy
   has_many :invitations
   has_and_belongs_to_many :events
+
+  validates :first_name, :last_name, :email, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
+  validate :has_a_bike
+
+  def has_a_bike
+    if [mens_bikes, womens_bikes, kids_bikes].reject(&:blank?).size == 0
+      errors[:base] << ('You need to add at least one bike to your order.')
+    end
+  end
+
 end
