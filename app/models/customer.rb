ab0d50f3419e24c9255da58764f1e343ba3ff9cd
mbd-s <michaelbsilverberg@gmail.com>
2016-10-04 16:57:38 +0200

    added email confirmation validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,6 @@ class Customer < ActiveRecord::Base
   has_and_belongs_to_many :events
 
   validates :first_name, :last_name, :email, presence: true
+  validates :email, :confirmation => true
+  validates :email_confirmation, :presence => true
 end
