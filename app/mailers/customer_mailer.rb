275df11d7257079b4489585a294a15fb2898ea69
mbd-s <michaelbsilverberg@gmail.com>
2016-10-19 17:03:05 +0200

    updated customer email confirmation mailer

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,9 +1,9 @@
 class CustomerMailer < ApplicationMailer
-  default from: 'welcome@rueckenwind-dev.com'
+  default from: 'rueckenwind.no.reply@gmail.com'
 
   def email_confirmation(customer)
     @url = "#{default_url_options[:scheme]}#{default_url_options[:host]}#{default_url_options[:port]}/confirm/#{customer.id}"
     @customer = customer
-    mail(to: @customer.email, subject: 'Thanks for your order.')
+    mail(to: @customer.email, subject: 'Welcome to Rückenwind! Please confirm your email')
   end
 end
