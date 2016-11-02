8c82de6912b7dd1d2d64fdc78c0830441e2249a6
mbd-s <michaelbsilverberg@gmail.com>
2016-11-02 18:03:10 +0100

    adds basic event crud and views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,53 @@
 class EventsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
     @events = Event.all
   end
 
   def new
     @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = "Event saved."
+      redirect_to events_path
+    else
+      flash[:error] = @event.errors.full_messages.to_sentence
+      redirect_to new_event_path
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      flash[:notice] = "Event updated."
+      redirect_to events_path
+    else
+      flash[:error] = @event.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    flash[:notice] = "Event deleted."
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :start_time, :end_time)
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ActiveRecord::Base
   has_many :volunteers
   has_many :customers
+
+  validates :date, :start_time, :end_time, presence: true
+
 end
