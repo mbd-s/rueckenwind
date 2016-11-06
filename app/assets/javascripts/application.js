cc5d0ffc1199d55e1ac852ff3b3f5b74797bbfe0
mbd-s <michaelbsilverberg@gmail.com>
2016-11-06 14:45:45 +0100

    grays out past dates in datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,6 @@
 $(document).ready(function(){
   $('select').material_select();
   $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 5 // Creates a dropdown of 5 years to control year
+    min: new Date(),
   });
 });
