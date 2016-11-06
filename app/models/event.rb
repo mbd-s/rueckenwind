70e229865e3ae06ed52965266e25fe8493843449
mbd-s <michaelbsilverberg@gmail.com>
2016-11-06 14:27:36 +0100

    adds time validations for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,14 @@ class Event < ActiveRecord::Base
   has_many :customers
 
   validates :date, :start_time, :end_time, presence: true
+  validate :date_cannot_be_in_the_past, :end_time_must_be_after_start_time
+  def date_cannot_be_in_the_past
+    errors.add(:date, "can't be in the past.") if
+      !date.blank? and date < Date.today
+  end
 
+  def end_time_must_be_after_start_time
+    errors.add(:end_time, "must be after start time.") if
+    end_time < start_time
+  end
 end
