41e2590804785610c9f9506a54d3c7121c66e0a5
mbd-s <michaelbsilverberg@gmail.com>
2016-11-23 17:00:04 +0100

    adds user management links and actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: "User deleted."
+  end
+
   private
 
   def user_params
