1bd90bfa6b195bf19fd3bdcb84bef0b63905dacd
mbd-s <michaelbsilverberg@gmail.com>
2016-11-29 18:54:32 +0100

    fixes td scope bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to root_path, notice: "User deleted."
+    redirect_to users_path, notice: "User deleted."
   end
 
   private
