5a702dc275c641b47391ab3e14bec761f7a12bcb
mbd-s <michaelbsilverberg@gmail.com>
2016-12-19 17:22:30 +0100

    makes seed admin more generic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.destroy_all
 
-a = User.create(
+User.create(
   first_name: "Seed",
   last_name: "Admin",
   email: "a@a",
@@ -9,16 +9,16 @@ a = User.create(
   reset_password_sent_at: nil,
   remember_created_at: nil,
   sign_in_count: 3,
-  current_sign_in_at: "2016-11-04 18:21:33",
-  last_sign_in_at: "2016-11-04 18:20:30",
+  current_sign_in_at: nil,
+  last_sign_in_at: nil,
   current_sign_in_ip: nil,
   last_sign_in_ip: nil,
   confirmation_token: "3pdxVjxo8tdZHGTCtwsC",
-  confirmed_at: "2016-11-04 18:13:59",
-  confirmation_sent_at: "2016-11-04 18:13:51",
+  confirmed_at: nil,
+  confirmation_sent_at: nil,
   unconfirmed_email: nil,
-  created_at: "2016-11-04 18:13:51",
-  updated_at: "2016-11-04 18:21:33",
+  created_at: Time.now,
+  updated_at: Time.now,
   role: "admin"
 )
 puts "Just created #{User.count} users"
