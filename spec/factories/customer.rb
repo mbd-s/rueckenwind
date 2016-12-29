bd95aa73e5d11b3fa7c98cb0b45591bc6706a0dc
mbd-s <michaelbsilverberg@gmail.com>
2016-12-29 16:23:01 +0100

    expands customer unit tests to check validations

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
   factory :customer do
-    first_name "Jack"
-    last_name "Customer"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    facebook "facebook.com/jack_customer"
+    has_donated false
+    signed_up false
+    wants_to_be_mechanic false
+    created_at Time.now
+    updated_at Time.now
+    email_confirmed true
+    confirm_token "oajsodfj5"
   end
 end
--- spec/unit/models/customer_spec.rb
+++ spec/unit/models/customer_spec.rb
@@ -7,6 +7,10 @@ describe Customer do
   it { should have_many :events}
   it { should have_many(:events).through(:orders) }
 
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+
   xit 'should remove related orders' do
     expect { customer.destroy }.to change { Order.count }.by(-1)
   end
