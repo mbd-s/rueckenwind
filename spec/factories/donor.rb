bee618f1dc4b5fe13e844f0a8a820a556e88537d
mbd-s <michaelbsilverberg@gmail.com>
2016-12-29 17:24:17 +0100

    incorporates faker into user and donor unit tests

--- spec/factories/donor.rb
+++ spec/factories/donor.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :donor do
-    first_name "Kelly"
-    last_name "Donor"
-    email "kellydonor@fake_email.com"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
     created_at Time.now
     updated_at Time.now
   end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    first_name "Bradley"
-    last_name "Volunteer"
-    email "bradley_user@fake_email.com"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
     created_at Time.now
     updated_at Time.now
     role "volunteer"
