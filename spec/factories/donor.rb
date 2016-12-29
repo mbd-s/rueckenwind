d10ef71ce517c947e5e60599c12bcf1c9a77004a
mbd-s <michaelbsilverberg@gmail.com>
2016-12-29 17:43:52 +0100

    adds values to test donor objects and checks for validations

--- spec/factories/donor.rb
+++ spec/factories/donor.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
+    phone { Faker::PhoneNumber.phone_number }
     created_at Time.now
     updated_at Time.now
   end
--- spec/unit/models/donor_spec.rb
+++ spec/unit/models/donor_spec.rb
@@ -4,5 +4,8 @@ describe Donor do
   let(:donor) { FactoryGirl.build :donor }
 
   it { should have_one(:donation) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
 
 end
