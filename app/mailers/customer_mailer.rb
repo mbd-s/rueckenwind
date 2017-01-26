e2606eb4ded9c00d10544b8ea2338a9da7a18d34
mbd-s <michaelbsilverberg@gmail.com>
2017-01-26 17:04:05 +0100

    adds logo and styling to email confirmation mailer

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -4,6 +4,7 @@ class CustomerMailer < ApplicationMailer
   def email_confirmation(customer)
     @url = "#{default_url_options[:scheme]}#{default_url_options[:host]}#{default_url_options[:separator]}#{default_url_options[:port]}/confirm/#{customer.confirm_token}"
     @customer = customer
+    attachments.inline['logo.png'] = File.read('app/assets/images/logo.png')
     mail(to: @customer.email, subject: 'Welcome to Rückenwind! Please confirm your email address')
   end
 
